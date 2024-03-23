43aa1aac489f32b71b1b953586748dfa3ace29c0
ArteMaxL <artemiolucero@gmail.com>
2024-03-23 17:00:01 -0300

    Add project files.

---
+++ Blog.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult SubmitTag() 
+        {
+            //// Captura de inputs de forma manual
+            //var name = Request.Form["name"];
+            //var displayName = Request.Form["displayName"];
+
+            return View("Add");
+        }
+    }
+}
