6557d2562e6e6715a1a7370834db58ebfa505b68
ArteMaxL <artemiolucero@gmail.com>
2024-03-23 21:50:40 -0300

    Finalizado CRUD de Tags

--- Blog.Web/Controllers/AdminTagsController.cs
+++ Blog.Web/Controllers/AdminTagsController.cs
@@ -1,10 +1,19 @@
-﻿using Blog.Web.Models.ViewModels;
+﻿using Blog.Web.Data;
+using Blog.Web.Models.Domain;
+using Blog.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private readonly BlogDbContext _context;
+
+        public AdminTagsController(BlogDbContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
@@ -13,16 +22,98 @@ namespace Blog.Web.Controllers
 
         [HttpPost]
         [ActionName("Add")]
-        public IActionResult Add(AddTagRequest addTagRequest) 
+        public IActionResult Add(AddTagRequest addTagRequest)
         {
-            //// Captura los inputs del form de forma manual
-            //var name = Request.Form["name"];
-            //var displayName = Request.Form["displayName"];
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,
+            };
+            _context.Tags.Add(tag);
+            _context.SaveChanges();
 
-            var name = addTagRequest.Name;
-            var displayName = addTagRequest.DisplayName;
+            return RedirectToAction("List");
+        }
 
-            return View("Add");
+        [HttpGet]
+        [ActionName("List")]
+        public IActionResult List()
+        {
+            var tags = _context.Tags.ToList();
+
+            return View(tags);
+        }
+
+        [HttpGet]
+        [ActionName("Edit")]
+        public IActionResult Edit(Guid id)
+        {
+            var tag = _context.Tags.FirstOrDefault(t => t.Id == id);
+
+            if (tag != null)
+            {
+                var editTagRequest = new EditTagRequest
+                {
+                    Id = tag.Id,
+                    Name = tag.Name,
+                    DisplayName = tag.DisplayName
+                };
+
+                return View(editTagRequest);
+            }
+
+            return View(null);
+        }
+
+        [HttpPost]
+        [ActionName("Edit")]
+        public IActionResult Edit(EditTagRequest editTagRequest)
+        {
+            var tag = new Tag
+            {
+                Id = editTagRequest.Id,
+                Name = editTagRequest.Name,
+                DisplayName = editTagRequest.DisplayName
+            };
+
+            var exisistingTag = _context.Tags.Find(tag.Id);
+
+            if (exisistingTag != null)
+            {
+                exisistingTag.Name = tag.Name;
+                exisistingTag.DisplayName = tag.DisplayName;
+
+                _context.SaveChanges();
+
+                // Show success notification
+                // return RedirectToAction("Edit", new { id = editTagRequest.Id});
+
+                return RedirectToAction("List");
+            }
+            // Show error notification
+            // return RedirectToAction("Edit", new { id = editTagRequest.Id});
+
+            return RedirectToAction("Add");
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult Delete(EditTagRequest editTagRequest)
+        {
+            var tag = _context.Tags.Find(editTagRequest.Id);
+
+            if (tag != null)
+            {
+                _context.Tags.Remove(tag);
+                _context.SaveChanges();
+
+                // Show a success notification
+
+                return RedirectToAction("List");
+            }
+            // Show an error notification
+
+            return RedirectToAction("Edit", new { id = editTagRequest.Id });
         }
     }
 }
